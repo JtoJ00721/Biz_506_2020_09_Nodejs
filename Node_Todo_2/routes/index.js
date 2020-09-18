2d6404c361cf2f0e422e383589853221876ab02c
JtoJ00721 <moonlf2000@naver.com>
2020-09-18 15:48:44 +0900

    몽고DB로 프로젝트 재현하기 과제

--- Node_Todo_2/routes/index.js
+++ Node_Todo_2/routes/index.js
@@ -1,9 +1,11 @@
-var express = require('express');
+var express = require("express");
+const { request } = require("../app");
 var router = express.Router();
+var mongoose = require("mongoose");
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Express" });
 });
 
 module.exports = router;
